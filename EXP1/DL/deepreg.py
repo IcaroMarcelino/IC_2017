751e70ccba158af5e99284f561b69645329692d9
IcaroMarcelino <icaro.marcelino@hotmail.com>
2017-03-31 14:32:55 -0300

    Experimentos GP/DL

---
+++ EXP1/DL/deepreg.py
@@ -0,0 +1,84 @@
+from __future__ import absolute_import,division,print_function
+
+from sklearn.model_selection import train_test_split
+from sklearn import metrics, preprocessing
+
+import tensorflow as tf
+import math
+import numpy as np
+import csv
+
+import time
+ran = [[[10,10,10,10,10,10,10], range(0,10)], [[10,10,10,10,10], range(10,20)], [[10,10,10], range(20,30)], [[10], range(30,40)], [[100000], range(40,50)]]
+step_samples = [(4, 10), (4, 20), (4,30), (4,50), (4, 100)]
+lim_inf = -5
+lim_sup = +5
+
+arq = 0
+for param in ran:
+	for step, scale in step_samples:
+		for i in param[1]:
+			start = time.time()
+			################ Samples #####################################
+			# X_f1 = list(np.random.uniform(-50,50,40000))
+			# Y_f1 = list(np.random.uniform(-50,50,40000))
+			# XY_f1 = list(zip(X_f1, Y_f1))
+
+			X_f1 = [x*(1/scale) for x in list(range(lim_inf*scale, lim_sup*scale, step))]
+			X_f1 = np.random.permutation(X_f1)
+			Y_f1 = [y*(1/scale) for y in list(range(lim_inf*scale, lim_sup*scale, step))]
+			Y_f1 = np.random.permutation(Y_f1)
+			XY_f1 = list(zip(X_f1, Y_f1))
+
+			############### Functions for regression #####################
+			def f1(x,y):
+				return (1/(1 + x**(-4))) + (1/(1 + y**(-4)))
+
+			XY_f1 = np.array(XY_f1, dtype = 'float32')
+			F1_XY = np.array([f1(x_,y_) for x_,y_ in XY_f1],
+							dtype = 'float32')
+
+			# Split dataset into train / test
+			x_train, x_test, y_train, y_test = train_test_split(XY_f1, F1_XY, test_size=0.3, random_state=42)
+
+			# Scale data (training set) to 0 mean and unit standard deviation.
+			#scaler = preprocessing.StandardScaler()
+			#x_train = scaler.fit_transform(x_train)
+
+			# Build 2 layer fully connected DNN with 10, 10 units respectively.
+			feature_columns = tf.contrib.learn.infer_real_valued_columns_from_input(x_train)
+			regressor = tf.contrib.learn.DNNRegressor(feature_columns=feature_columns, hidden_units=param[0])
+
+			# Fit
+			regressor.fit(x = x_train, y = y_train, steps = 1000, batch_size = 32, input_fn = None, monitors = None, max_steps = None)
+
+			# Transform
+			#x_transformed = scaler.transform(x_test)
+			x_transformed = x_test
+
+			# Predict and score
+			y_predicted = list(regressor.predict(x_transformed, as_iterable=True))
+			score = metrics.mean_squared_error(y_predicted, y_test)
+
+			xv = []
+			result = []
+			for count in range(len(X_f1)):
+				xv.append([X_f1[count], Y_f1[count]])
+				result.append(f1(XY_f1[count][0], XY_f1[count][1]))
+			xv = np.array(xv, dtype = 'float32')
+
+			end = time.time()
+			print('MSE: {0:f}'.format(score))
+			info = open("INFO_DL_EXP1.csv", 'a')
+			info.write(str(arq +1) + ',' + str(len(XY_f1)) + ',' + str(i) + ',' + '{0:f}'.format(score) + ',' + str(end-start) + '\n')
+
+			tabela = list(zip(X_f1, Y_f1, result, list(regressor.predict(x = xv, as_iterable=True))))
+			tabela = [('x', 'y', "f(x,y)", "f*(x,y)")] + tabela
+
+			myfile = open("Saidas/SAIDA_EXP1_DL_ARQ" + str(arq +1) + "_Samples" + str(len(XY_f1)) + "_EXEC" + str(i) + ".csv", 'w')
+			wr = csv.writer(myfile)
+
+			for row in tabela:
+				wr.writerow(row)
+	arq += 1
+				
---
+++ EXP2/GP/symbreg.py
@@ -0,0 +1,237 @@
+from __future__ import absolute_import,division,print_function
+import operator
+import math
+import random
+
+import numpy
+
+from deap import algorithms
+from deap import base
+from deap import creator
+from deap import tools
+from deap import gp
+import numpy as np
+import pygraphviz as pgv
+from sympy import simplify, expand
+
+import time
+import csv
+
+def main(NEXEC, TAM_MAX, pointsX, pointsY, NGEN, CXPB, MUTPB, NPOP, train_percent, verb, FILE_NAME):
+	# Define new functions
+	def div(left, right):
+		try:
+			return left / right
+		except ZeroDivisionError:
+			return 1
+
+	########## Operator Set #########################################
+	pset = gp.PrimitiveSet("MAIN", 2)
+	pset.addPrimitive(operator.add, 2)
+	pset.addPrimitive(operator.sub, 2)
+	pset.addPrimitive(operator.mul, 2)
+	pset.addPrimitive(div, 2)
+	pset.addPrimitive(operator.neg, 1)
+	pset.addPrimitive(math.cos, 1)
+	pset.addPrimitive(math.sin, 1)
+	pset.addTerminal(1)
+	pset.addTerminal(2)
+	pset.renameArguments(ARG0='x')
+	pset.renameArguments(ARG1='y')
+	#################################################################
+
+	creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+	creator.create("Individual", gp.PrimitiveTree, fitness=creator.FitnessMin)
+	toolbox = base.Toolbox()
+	toolbox.register("expr", gp.genHalfAndHalf, pset=pset, min_=1, max_=2)
+	toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.expr)
+	toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+	toolbox.register("compile", gp.compile, pset=pset)
+
+	# def f(x,y):
+	# 	return (1/(1 + x**(-4))) + (1/(1 + y**(-4)))
+
+	def f(x,y):
+		return 2 - 2.1*math.sin(1.3*x)*math.cos(9.8*y)
+
+	def evalSymbReg(individual, points):
+		# Transform the tree expression in a callable function
+		f_approx = toolbox.compile(expr=individual)
+		# Evaluate the mean squared error between the expression and the real function
+		# sqerrors = ((f_approx(x,y) - f(x,y))**2 for x,y in points)
+		
+		sqerrors = []
+		for x,y in points:
+			try:
+				F1 = f_approx(x,y)
+			except ValueError:
+				F1 = 1e30
+			
+			sqerrors.append((F1 - f(x,y))**2)
+			
+		return math.fsum(sqerrors) / len(points),
+
+
+	points = zip(pointsX, pointsY)
+
+	TRAIN_TAM = int(train_percent*len(points))
+	TEST_TAM = int((1 - train_percent)*len(points))
+
+	toolbox.register("evaluate", evalSymbReg, points = points[:TRAIN_TAM])
+	toolbox.register("select", tools.selTournament, tournsize=3)
+	toolbox.register("mate", gp.cxOnePoint)
+	toolbox.register("expr_mut", gp.genFull, min_=1, max_=2)
+	toolbox.register("mutate", gp.mutUniform, expr=toolbox.expr_mut, pset=pset)
+
+	toolbox.decorate("mate", gp.staticLimit(key=operator.attrgetter("height"), max_value = TAM_MAX))
+	toolbox.decorate("mutate", gp.staticLimit(key=operator.attrgetter("height"), max_value = TAM_MAX))
+
+	start = time.time()
+	random.seed(318)
+
+	# def melhor(n, hof_true):
+	# 	hof = tools.HallOfFame(n)
+		
+	# 	if (len(hof)>0):
+	# 		if (hof[0].fitness.values[0] == 'nan')&(hof_true != []):
+	# 			return [hof_true]
+
+	# 	hof_true = hof
+	# 	return hof
+
+	# hof_true = []
+	# hof = melhor(1,hof_true)
+
+	stats_fit = tools.Statistics(lambda ind: ind.fitness.values)
+	stats_size = tools.Statistics(lambda ind: ind.height)
+	mstats = tools.MultiStatistics(fitness=stats_fit, size=stats_size)
+	mstats.register("avg", numpy.mean)
+	mstats.register("std", numpy.std)
+	mstats.register("min", numpy.min)
+	mstats.register("max", numpy.max)
+
+	pop = toolbox.population(NPOP)
+	
+	# pop, log = algorithms.eaSimple(population = pop, toolbox = toolbox, cxpb = CXPB, mutpb = MUTPB, ngen = NGEN, stats = mstats,
+	# 							   halloffame = hof, verbose = True)
+	
+	fitnesses = list(map(toolbox.evaluate, pop))
+	for ind, fit in zip(pop, fitnesses):
+		ind.fitness.values = fit
+		if (math.isnan(fit[0])):
+				ind.fitness.values = 1e30,
+		else:
+			ind.fitness.values = fit
+
+	log = tools.Logbook()
+	hof = tools.selBest(pop, 1)
+
+	print(">> Regressao Numerica com GP: ")
+	print(">> " + str(len(points)) + " amostras;" + "tamanho max da arvore: " + str(TAM_MAX))
+	print(">> Inicio (Execucao " + str(NEXEC) +")")
+	for g in range(NGEN):
+		geninit = time.time()
+		pop = toolbox.select(pop, len(pop))
+
+		offspring = algorithms.varAnd(pop, toolbox, CXPB, MUTPB)
+
+		fitnesses = list(map(toolbox.evaluate, offspring))
+		for ind, fit in zip(offspring, fitnesses):
+			if (math.isnan(fit[0])):
+				ind.fitness.values = 1e30,
+			else:
+				ind.fitness.values = fit
+
+		hof = tools.selBest(pop, 1)
+		pop[:] = offspring + hof
+
+		log.record(gen = g, time = time.time() - geninit,**mstats.compile(pop))
+
+		if(verb == True):
+			print(log.stream)
+
+	end = time.time()
+
+	logfile = open("Log_Exec/LOG_" + filename + "_" + str(NEXEC + 1) + ".csv", 'w')
+	logfile.write(str(log))
+	print(">> Fim da execucao (" + str(end - start) + " segundos)\n")
+
+
+	# final_eq = expand(simplify(convertFunct(hof[0])))
+	# print("Resultado da regressao: %s\n" % final_eq)
+
+	tree = gp.PrimitiveTree(hof[0])
+	function = gp.compile(tree, pset)
+	expFILE = open("Grafos_Melhores/EXPR_" + filename + "_" +  str(NEXEC + 1) + ".txt", 'w')
+	expFILE.write(str(tree))
+	
+	px_test = pointsX[TEST_TAM:]
+	py_test = pointsY[TEST_TAM:]
+	f_xy = [f(x,y) for x,y in points[TEST_TAM:]]
+
+	f_xy_approx = []
+	for x,y in points[TEST_TAM:]:
+		try:
+			F1 = function(x,y)
+		except ValueError:
+			F1 = float('nan')
+		f_xy_approx.append(F1)
+
+
+	tabela = list(zip(px_test, py_test, f_xy, f_xy_approx))
+	tabela = [('x', 'y', "f(x,y)", "f*(x,y)")] + tabela
+
+	saida = open("Saidas/SAIDA_" + filename + "_" + str(NEXEC + 1) + ".csv", 'w')
+	wr = csv.writer(saida)
+	for row in tabela:
+		wr.writerow(row)
+
+	info = open("Info/INFO_" + filename + ".csv", 'a')
+	if (NEXEC == 0):
+		info.write("Altura Maxima,#Amostras,#Execucao,MSE (Melhor),Altura (Melhor),Tempo Execucao\n")
+
+	info.write(str(TAM_MAX) + ',' + str(len(points)) + ',' +  str(NEXEC + 1) + ',' + str(toolbox.evaluate(hof[0])[0]) + ',' + str(hof[0].height) + ',' + str(end-start) + '\n')
+
+	info1 = open("INFO_GP_EXP1.csv", 'a')
+	info1.write(str(TAM_MAX) + ',' + str(len(points)) + ',' +  str(NEXEC + 1) + ',' + str(toolbox.evaluate(hof[0])[0]) + ',' + str(hof[0].height) + ',' + str(end-start) + '\n')
+
+	nodes, edges, labels = gp.graph(hof[0])
+
+	g = pgv.AGraph()
+	g.add_nodes_from(nodes)
+	g.add_edges_from(edges)
+	g.layout(prog="dot")
+
+	for i in nodes:
+		n = g.get_node(i)
+		n.attr["label"] = labels[i]
+
+	g.draw("Grafos_Melhores/GRAPF_" + filename +  "_" + str(NEXEC + 1) + ".pdf")
+	hof = []
+
+if __name__ == "__main__":
+	NGEN = 100
+	CXPB = .85
+	MUTPB = .15
+	NPOP = 300
+	train_percent = 0.7
+
+	tam_max_tree = [5,10,17,34,68,90]
+	step_samples = [(4, 10), (4, 20), (4,30), (4,50), (4, 100)]
+
+	for tam_max in tam_max_tree:
+		for step, scale in step_samples:
+			for n in list(range(0,10)):
+				lim_inf = -5
+				lim_sup = +5
+
+				px = [x*(1/scale) for x in list(range(lim_inf*scale, lim_sup*scale, step))]
+				px = np.random.permutation(px)
+
+				py = [y*(1/scale) for y in list(range(lim_inf*scale, lim_sup*scale, step))]
+				py = np.random.permutation(py)
+
+				filename = "GP_TreeMax" + str(tam_max) + "_Samples" + str(len(px))
+
+				main(	NEXEC = n, 		TAM_MAX = tam_max, 	pointsX = px, 	pointsY = py, 	NGEN = NGEN,	CXPB = CXPB, 	
+						MUTPB = MUTPB, 		NPOP = NPOP, 	train_percent = train_percent, 	verb = False,	FILE_NAME = filename)
